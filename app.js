25a1d3f60056329df57d3f487b61f7c3a7606553
gouz7514 <gouz7514@gmail.com>
2022-09-23 09:09:03 +0900

    Update : array

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+// const person: {
+//   name: string
+//   age: number
+// } = {
+//   name: 'Kim',
+//   age: 30
+// }
 var person = {
     name: 'Kim',
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
 };
+var favoriteActivites;
+favoriteActivites = ['sports'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- app.ts
+++ app.ts
@@ -1,9 +1,22 @@
-const person: {
-  name: string
-  age: number
-} = {
+// const person: {
+//   name: string
+//   age: number
+// } = {
+//   name: 'Kim',
+//   age: 30
+// }
+
+const person = {
   name: 'Kim',
-  age: 30
+  age: 30,
+  hobbies: ['Sports', 'Cooking']
 }
 
+let favoriteActivites: string[]
+favoriteActivites = ['sports']
+
 console.log(person.name)
+
+for(const hobby of person.hobbies) {
+  console.log(hobby)
+}
