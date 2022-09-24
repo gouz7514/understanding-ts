d1724de084a77384617ef129a71ad7b30fd60dda
gouz7514 <gouz7514@gmail.com>
2022-09-24 11:03:27 +0900

    Update : enum

--- app.js
+++ app.js
@@ -5,17 +5,39 @@
 //   name: 'Kim',
 //   age: 30
 // }
+// const person : {
+//   name: string
+//   age: number
+//   hobbies: string[]
+//   role: [number, string]
+// } = {
+//   name: 'Kim',
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author']
+// }
+// const ADMIN = 0
+// const READ_ONLY = 1
+// const AUTHOR = 2
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 5] = "ADMIN";
+    Role[Role["READ_ONLY"] = 6] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 7] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: 'Kim',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN
 };
-person.role.push('admin');
 var favoriteActivites;
 favoriteActivites = ['sports'];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby);
+}
+if (person.role === Role.ADMIN) {
+    console.log('is admin');
 }
--- app.ts
+++ app.ts
@@ -6,19 +6,30 @@
 //   age: 30
 // }
 
-const person : {
-  name: string
-  age: number
-  hobbies: string[]
-  role: [number, string]
-} = {
+// const person : {
+//   name: string
+//   age: number
+//   hobbies: string[]
+//   role: [number, string]
+// } = {
+//   name: 'Kim',
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author']
+// }
+
+// const ADMIN = 0
+// const READ_ONLY = 1
+// const AUTHOR = 2
+
+enum Role { ADMIN = 5, READ_ONLY, AUTHOR }
+
+const person = {
   name: 'Kim',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author']
+  role: Role.ADMIN
 }
-
-person.role.push('admin')
 
 let favoriteActivites: string[]
 favoriteActivites = ['sports']
@@ -27,4 +38,8 @@ console.log(person.name)
 
 for(const hobby of person.hobbies) {
   console.log(hobby)
+}
+
+if (person.role === Role.ADMIN) {
+  console.log('is admin')
 }
