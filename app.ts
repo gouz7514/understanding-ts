f30b878421182467a0b16041f60f8464e7477ff2
gouz7514 <gouz7514@gmail.com>
2022-09-29 23:13:12 +0900

    Update : void, undefined

--- app.ts
+++ app.ts
@@ -1,32 +1,10 @@
-type Combinable = number | string
-type ConversionDescriptor = 'as-number' | 'as-text'
-
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: ConversionDescriptor
-) {
-  let result
-  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-    result = +input1 + +input2
-  } else {
-    result = input1.toString() + input2.toString()
-  }
-
-  return result
-
-  // if (resultConversion === 'as-number') {
-  //   return +result
-  // } else {
-  //   return result.toString()
-  // }
+function add(n1: number, n2: number) {
+  return n1 + n2
 }
 
-const combinedAges = combine(30, 26, 'as-number')
-console.log(combinedAges)
+function printResult(num: number) {
+  console.log('Result : ' + num)
+}
 
-const cominbedStringAges = combine('30', '26', 'as-number')
-console.log(cominbedStringAges)
-
-const cominbedNames = combine('Max', 'Anna', 'as-text')
-console.log(cominbedNames)
+printResult(add(5, 12))
+console.log(printResult(add(5, 12)))
