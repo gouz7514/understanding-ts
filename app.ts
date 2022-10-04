72b90ca854e8e9abd4a783324bdcc6aa8db38cbd
gouz7514 <gouz7514@gmail.com>
2022-10-04 22:14:46 +0900

    Update : 타입의 기능을 하는 함수

--- app.ts
+++ app.ts
@@ -7,4 +7,10 @@ function printResult(num: number) {
 }
 
 printResult(add(5, 12))
-console.log(printResult(add(5, 12)))
+
+let combineValues: (a: number, b: number) => number
+
+combineValues = add
+// combineValues = printResult
+
+console.log(combineValues(8, 8))
