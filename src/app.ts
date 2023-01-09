6e545a82082aef4bd6d3dbfb9fcaf7b60d4a3525
gouz7514 <gouz7514@gmail.com>
2023-01-09 22:36:06 +0900

    Update : abstract

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-class Department {
+abstract class Department {
   static fiscalYear = 2023
   // private id: string
   // private name: string
   protected employees: string[] = []
 
-  constructor(private readonly id: string, public name: string) {
+  constructor(protected readonly id: string, public name: string) {
     // this.id = id
     // this.name = n
   }
@@ -15,9 +15,7 @@ class Department {
     }
   }
 
-  describe(this: Department) {
-    console.log(`Department (${this.id}) : `, this.name)
-  }
+  abstract describe(this: Department): void
 
   addEmployee(employee: string) {
     this.employees.push(employee)
@@ -35,6 +33,10 @@ class ITDepartment extends Department {
   constructor(id: string, admins: string[]) {
     super(id, 'IT')
     this.admins = admins
+  }
+
+  describe() {
+    console.log('IT Department ID : ', this.id)
   }
 }
 
@@ -58,6 +60,10 @@ class AccountingDepartment extends Department {
   constructor(id: string, private reports: string[]) {
     super(id, 'Accounting')
     this.lastReport = reports[0]
+  }
+
+  describe() {
+    console.log('Accounting Deparmment - ID : ', this.id)
   }
 
   addEmployee(name: string) {
@@ -96,8 +102,9 @@ accounting.getReports()
 
 accounting.addEmployee('Kim')
 accounting.addEmployee('Lee')
+accounting.describe()
 
-accounting.printEmployeeInformation()
+// accounting.printEmployeeInformation()
 
 // const accountingCopy = {
 //   name: 's',
