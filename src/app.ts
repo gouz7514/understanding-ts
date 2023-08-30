a831ba0ffa2beeff65c33438f0a1520f9244e6e3
gouz7514 <gouz7514@gmail.com>
2023-08-30 11:13:32 +0900

    Update : decorator practice

--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,84 @@
-// type Sumfn = (A: number, b: number) => number
-interface SumFn {
-  (A: number, b: number): number
+function Logger(logString: string) {
+  console.log('LOGGER FACTORY')
+  return function(constructor: Function) {
+    console.log(logString)
+    console.log(constructor)
+  }
 }
 
-let sum: SumFn
-
-sum = (n1: number, n2: number) => {
-  return n1 + n2
-}
-
-interface Named {
-  readonly name?: string
-  outputName?: string
-}
-
-interface Greetable extends Named {
-  greet(phrase: string): void
-}
-
-class Person implements Greetable, Named {
-  name?: string
-  age = 30
-
-  constructor(n: string = '') {
-    if (n) {
-      this.name = n
+function WithTemplate(template: string, hookId: string) {
+  console.log('TEMPLATE FACTORY')
+  return function(constructor: any) {
+    console.log('Rendering template')
+    const hookEl = document.getElementById(hookId)
+    const p = new constructor()
+    if (hookEl) {
+      hookEl.innerHTML = template
+      hookEl.querySelector('h1')!.textContent = p.name
     }
   }
+}
 
-  greet(phrase: string) {
-    if (this.name) {
-      console.log(phrase + ' ' + this.name)
+@Logger('LOGGING - PERSON')
+@WithTemplate('<h1>My Person Object</h1>', 'app')
+class Person {
+  name = 'Kim'
+
+  constructor() {
+    console.log('Creating person object...')
+  }
+}
+
+// const person = new Person()
+
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log('Property decorator!')
+  console.log(target, propertyName)
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log('Accessor decorator!')
+  console.log(target)
+  console.log(name)
+  console.log(descriptor)
+}
+
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+  console.log('Method decorator!')
+  console.log(target)
+  console.log(name)
+  console.log(descriptor)
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log('Parameter decorator!')
+  console.log(target)
+  console.log(name)
+  console.log(position)
+}
+
+class Product {
+  @Log
+  title: string
+  private _price: number
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this.price = val
     } else {
-      console.log('Hi')
+      throw new Error('Invalid price - should be positive!')
     }
   }
+
+  constructor(t: string, p: number) {
+    this.title = t
+    this._price = p
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax)
+  }
 }
-
-let user1: Greetable
-
-user1 = new Person()
-
-user1.greet('Hello')
-console.log(user1)
