449f133f495c8971fc6ce29888a6321f84385da1
gouz7514 <gouz7514@gmail.com>
2023-08-30 11:44:40 +0900

    Update : this를 바인딩하는 데코레이터 만들기

--- src/app.ts
+++ src/app.ts
@@ -86,3 +86,32 @@ class Product {
     return this._price * (1 + tax)
   }
 }
+
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    // 여기서 this는 getter를 호출하는 객체를 가리킨다.
+    get() {
+      const boundFn = originalMethod.bind(this)
+      return boundFn
+    }
+  }
+  return adjDescriptor
+}
+
+class Printer {
+  message = 'This works!'
+
+  @Autobind
+  showMessage() {
+    console.log(this.message)
+  }
+}
+
+const p = new Printer()
+p.showMessage()
+
+const button = document.querySelector('button')!
+button.addEventListener('click', p.showMessage)
