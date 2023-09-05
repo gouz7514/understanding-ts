440a064a3e08df2a6ceea548a1d21cca34937f6c
gouz7514 <gouz7514@gmail.com>
2023-09-05 11:59:02 +0900

    Update : Project class, 타입 정의 생성 및 사용

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,22 @@
+// Project Type
+enum ProjectStatus { Active, Finished }
+
+class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public people: number,
+    public status: ProjectStatus
+  ) {}
+}
+
 // Project state Management
+type Listener = (items: Project[]) => void
+
 class ProjectState {
-  private listeners: any[] = []
-  private projects: any[] = []
+  private listeners: Listener[] = []
+  private projects: Project[] = []
   private static instance: ProjectState
 
   // singleton
@@ -15,17 +30,20 @@ class ProjectState {
     return this.instance
   }
 
-  addListener(listenerFn: Function) {
+  addListener(listenerFn: Listener) {
     this.listeners.push(listenerFn)
   }
 
   addProject(title: string, description: string, numOfPeople: number) {
-    const newProject = {
-      id: Math.random().toString(),
+    const newProject = new Project(
+      Math.random().toString(),
       title,
       description,
-      people: numOfPeople
-    }
+      numOfPeople,
+      ProjectStatus.Active
+    )
+
+    console.log(newProject)
 
     this.projects.push(newProject)
 
@@ -86,7 +104,7 @@ class ProjectList {
   templateElement: HTMLTemplateElement
   hostElement: HTMLDivElement
   element: HTMLElement // this will be a section element
-  assignedProjects: any[]
+  assignedProjects: Project[]
 
   constructor(private type: 'active' | 'finished') {
     this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement
@@ -97,7 +115,7 @@ class ProjectList {
     this.element = importedNode.firstElementChild as HTMLElement
     this.element.id = `${this.type}-projects`
 
-    projectState.addListener((projects: any) => {
+    projectState.addListener((projects: Project[]) => {
       this.assignedProjects = projects
       this.renderProjects()
     })
