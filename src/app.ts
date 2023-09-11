cbf6c9e458de7d51db471b6f628f530aa79417fd
gouz7514 <gouz7514@gmail.com>
2023-09-11 20:20:01 +0900

    Update : 드래그 앤 드롭 마무리

--- src/app.ts
+++ src/app.ts
@@ -59,7 +59,18 @@ class ProjectState extends State<Project> {
     )
 
     this.projects.push(newProject)
+    this.updateListeners()
+  }
 
+  moveProject(projectId: string, newStatus: ProjectStatus) {
+    const project = this.projects.find(project => project.id === projectId)
+    if (project && project.status !== newStatus) {
+      project.status = newStatus
+      this.updateListeners()
+    }
+  }
+
+  private updateListeners() {
     for (const listenerFn of this.listeners) {
       listenerFn(this.projects.slice())
     }
@@ -203,8 +214,10 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
     }
   }
 
+  @autobind
   dropHandler(event: DragEvent): void {
-    console.log(event.dataTransfer!.getData('text/plain'))
+    const prjId = event.dataTransfer!.getData('text/plain')
+    projectState.moveProject(prjId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished)
   }
 
   @autobind
