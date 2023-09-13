2e085f27656b63a1131461adb56ea08aead7d739
gouz7514 <gouz7514@gmail.com>
2023-09-13 15:40:59 +0900

    Update : reference 대신 import, export 사용하기

--- src/decorators/autobind.ts
+++ src/decorators/autobind.ts
@@ -1,15 +1,13 @@
-namespace App {
   // autobind decorator
-  export function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
-    const originalMethod = descriptor.value
-    const adjustedDescriptor: PropertyDescriptor = {
-      configurable: true,
-      get() {
-        const boundFn = originalMethod.bind(this)
-        return boundFn
-      }
+export function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value
+  const adjustedDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this)
+      return boundFn
     }
-  
-    return adjustedDescriptor
   }
+
+  return adjustedDescriptor
 }
